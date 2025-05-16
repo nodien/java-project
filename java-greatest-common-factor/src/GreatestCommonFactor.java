b04b8b79d130717b355e346e75c5d28f2c68ba56
Dien Nguyen Ba <nbdien2110@gmail.com>
2025-05-16 19:18:17 +0700

    greatest common factor ex done

---
+++ java-greatest-common-factor/src/GreatestCommonFactor.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class GreatestCommonFactor {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        a = sc.nextInt();
+        System.out.print("Enter b: ");
+        b = sc.nextInt();
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        if (a ==0 || b ==0) {
+            System.out.println("No greatest common factor");
+        }
+
+        while (a != b) {
+            if (a > b) {
+                a= a-b;
+            } else {
+                b=b-a;
+            }
+        }
+
+        System.out.println("Greatest common factor is: " + a);
+    }
+}
